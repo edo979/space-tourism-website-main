0a89a83b5361e4e22eafb1276be66bfa4cc5a8b8
edo979 <prezentabilan@gmail.com>
2022-05-03 18:25:59 +0200

    get tabs name using fetch api

--- destinationScript.js
+++ destinationScript.js
@@ -3,39 +3,15 @@
 const tabListEl = document.querySelector('[role="tablist"]')
 
 let currentTab = 0
-const tabCount = tabListEl.childElementCount
+init()
 
-tabListEl.addEventListener('keydown', (e) => {
-  if (e.keyCode === 37 || e.keyCode === 39) {
-    const keyRight = 39 // keyLeft = 37
+async function init() {
+  const data = await getContent('destinations')
 
-    tabListEl.children[currentTab].setAttribute('tabindex', '-1')
+  setContent(data)
+}
 
-    if (e.keyCode == keyRight) {
-      currentTab++
-      if (currentTab >= tabCount) {
-        currentTab = 0
-      }
-    } else {
-      currentTab--
-      if (currentTab < 0) {
-        currentTab = tabCount - 1
-      }
-    }
-
-    tabListEl.children[currentTab].setAttribute('tabindex', '0')
-    tabListEl.children[currentTab].focus()
-  }
-})
-
-Array.from(tabListEl.children).forEach((tab, i) =>
-  tab.addEventListener('click', (e) => {
-    currentTab = i
-    setActiveTab(e.target)
-  })
-)
-
-function setActiveTab(clickedTab) {
+async function setActiveTab(clickedTab) {
   Array.from(tabListEl.children).forEach((tab) => {
     tab.setAttribute('aria-selected', false)
     tab.setAttribute('tabindex', '-1')
@@ -45,4 +21,74 @@ function setActiveTab(clickedTab) {
   clickedTab.setAttribute('tabindex', '0')
 }
 
-//function setTabContent() {}
+async function getContent(contentCategory) {
+  try {
+    const response = await fetch('assets/data.json')
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    return data[contentCategory]
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+}
+
+function setContent(data) {
+  setTabsName(data.map((destination) => destination.name))
+  setEventListeners()
+}
+
+function setTabsName(names) {
+  let tabsName = ''
+
+  names.forEach((name, i) => {
+    tabsName += `<button
+      aria-selected="${i == 0 ? true : false}"
+      role="tab"
+      aria-controls="${name.toLowerCase()}-tab"
+      tabindex="${i == 0 ? '0' : '-1'}"
+    >
+      ${name}
+    </button>`
+  })
+
+  tabListEl.innerHTML = tabsName
+}
+
+function setEventListeners() {
+  const tabCount = tabListEl.childElementCount
+
+  tabListEl.addEventListener('keydown', (e) => {
+    if (e.keyCode === 37 || e.keyCode === 39) {
+      const keyRight = 39 // keyLeft = 37
+
+      tabListEl.children[currentTab].setAttribute('tabindex', '-1')
+
+      if (e.keyCode == keyRight) {
+        currentTab++
+        if (currentTab >= tabCount) {
+          currentTab = 0
+        }
+      } else {
+        currentTab--
+        if (currentTab < 0) {
+          currentTab = tabCount - 1
+        }
+      }
+
+      tabListEl.children[currentTab].setAttribute('tabindex', '0')
+      tabListEl.children[currentTab].focus()
+    }
+  })
+
+  Array.from(tabListEl.children).forEach((tab, i) =>
+    tab.addEventListener('click', (e) => {
+      currentTab = i
+      setActiveTab(e.target)
+    })
+  )
+}
